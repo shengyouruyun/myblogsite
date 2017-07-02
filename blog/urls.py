28e1f405b2322edaa9042998ee17b39603f1a6e5
Toby Wang <1479779020@qq.com>
2017-07-02 14:48:50 +0800

    mysite4.0 add complete

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
-#from . import views
+
 from blog.views import post_list, post_details, post_share
 urlpatterns = [
-# post views
+
 url(r'^tag/(?P<tag_slug>[-\w]+)/$', post_list, name='post_list_by_tag'),
 
-url(r'^(?P<post>[-\w]+)/$', 
-	post_details, name='post_details'),
-
+url(r'^(?P<post>[-\w]+)/$', post_details, name='post_details'),
 url(r'^(?P<post_id>\d+)/share/$', post_share, name='post_share'),
 url(r'^$', post_list, name='post_list'),
+
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap 
+from blog.feeds import LatestPostsFeed
+sitemaps = {
+'posts': PostSitemap,
+}
 
+feed = LatestPostsFeed()
 admin.autodiscover()
 urlpatterns = [
     # Examples:
@@ -12,4 +19,9 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls',
 								namespace='blog',
 								app_name='blog')),
+
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+name='django.contrib.sitemaps.views.sitemap'),
+
+    url(r'^blog/feed\.xml$', feed, name='post_feed'),
 ]
